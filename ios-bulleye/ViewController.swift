90d1bd45db3c1bbd011b8f45dfbb4d3f60e31d12
Chaofan Zhang <chaofan.zhang@ef.com>
2016-10-27 18:27:04 +0800

    First commit

---
+++ ios-bulleye/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ios-bulleye
+//
+//  Created by Chaofan Zhang on 10/27/16.
+//  Copyright © 2016 Chaofan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
